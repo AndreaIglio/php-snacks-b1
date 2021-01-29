26cd0bb4c6ef7ff2f4aa351d14a1faaed2adeacc
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-29 19:18:15 +0100

    Snack 2

--- snack2.php
+++ snack2.php
@@ -10,6 +10,32 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 
 */
 
+$name = $_GET['name'];
+$email = $_GET['email'];
+$age = $_GET['age'];
+
+
+if(strlen($name)> 3 && strpos($email, "@") && strpos($email, ".") && (is_numeric($age)) ){
+    var_dump('You can access');
+}
+else {
+    var_dump('You cannot access');
+}
+
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+</body>
+</html>
